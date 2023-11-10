d151ab04e47607338d67f9c04c3d41e2fc136bcf
jzhang319 <jzhang319@gmail.com>
2023-11-10 12:34:41 -0500

    fiished uncompressed

---
+++ 001uncompressed.js
@@ -0,0 +1,20 @@
+const uncompress = (s) => {
+  // todo
+  // console.log(s)
+  numbers = '0123456789'
+  let result = ''
+  let i = 0, j = 0
+  while (j < s.length){
+    if (numbers.includes(s[j])){
+      j+=1
+    } else {
+      const num = Number(s.slice(i, j))
+      for ( let count = 0; count < num; count++){
+        result += s[j]
+      }
+      j += 1
+      i = j
+    }
+  }
+  return result
+};

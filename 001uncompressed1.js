19c3f328b383d9f36afb74867d647757a137c1e1
jzhang319 <jzhang319@gmail.com>
2023-11-10 12:41:18 -0500

    finished 001 with updated performance

---
+++ 001uncompressed1.js
@@ -0,0 +1,18 @@
+const uncompress = (s) => {
+  numbers = '0123456789'
+  let result = []
+  let i = 0, j = 0;
+  while (j < s.length) {
+    if (numbers.includes(s[j])){
+      j += 1;
+    } else {
+      const num = Number(s.slice(i, j))
+      for (let count = 0; count < num; count++){
+        result.push(s[j])
+      }
+    }
+  }
+  return result.join('')
+}
+
+// improve performance by using array instead of concatenating strings

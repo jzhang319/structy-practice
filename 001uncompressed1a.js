05c387ea91c653fc054e7ce74154c0bc8f24ee80
jzhang319 <jzhang319@gmail.com>
2023-11-12 00:12:12 -0500

    solving 001 again

--- 001uncompressed1a.js
+++ 001uncompressed1a.js
@@ -1,24 +1,22 @@
 const uncompress = (s) => {
-  const numbers = '0123456789'
-  let result = []
-  let i = 0, j = 0;
+  const numbers = "0123456789";
+  let result = [];
+  let i = 0,
+    j = 0;
   while (j < s.length) {
-    if (numbers.includes(s[j])){
+    if (numbers.includes(s[j])) {
       j += 1;
     } else {
-      const num = Number(s.slice(i, j))
-      for (let count = 0; count < num; count++){
-        result.push(s[j])
+      const num = Number(s.slice(i, j));
+      for (let count = 0; count < num; count++) {
+        result.push(s[j]);
       }
       j += 1;
       i = j;
     }
   }
-  return result.join('')
-}
-
-// improve performance by using array instead of concatenating strings
-
+  return result.join("");
+};
 
 // example 1
 console.log(uncompress("2c3a1t")); // -> 'ccaaat')

2630ca00097ea1965feb242b7387b27e1094b36e
jzhang319 <jzhang319@gmail.com>
2023-11-12 22:18:54 -0500

    solved 003

--- 003isAnagram.js
+++ 003isAnagram.js
@@ -1,26 +1,30 @@
-
-
 const anagrams = (s1, s2) => {
   // todo
-  const map = new Map();
-  if (s1.length!== s2.length) {
+  const map = {};
+  if (s1.length !== s2.length) {
     return false;
   }
-  for (let i = 0; i < s1.length; i++) {
-    if (map.has(s1[i])) {
-      map.set(s1[i], map.get(s1[i]) + 1);
-    } else {
-      map.set(s1[i], 1);
+  for (let char of s1) {
+    if (!(char in map)) {
+      map[char] = 0;
     }
-    if (map.has(s2[i])) {
-      map.set(s2[i], map.get(s2[i]) - 1);
+    map[char] += 1;
+  }
+  for (let char of s2) {
+    if (char in map) {
+      map[char] -= 1;
     } else {
-      map.set(s2[i], -1);
+      return false;
     }
   }
-  
+  for (let char in map) {
+    if (map[char] !== 0) {
+      return false;
+    }
+  }
+  return true;
 };
 
-console.log(anagrams('restful', 'fluster')); // -> true
-console.log(anagrams('cats', 'tocs')); // -> false
-console.log(anagrams('monkeyswrite', 'newyorktimes')); // -> true
+console.log(anagrams("restful", "fluster")); // -> true
+console.log(anagrams("cats", "tocs")); // -> false
+console.log(anagrams("monkeyswrite", "newyorktimes")); // -> true

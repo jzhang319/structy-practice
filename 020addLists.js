dd496f0cbaecf1c2d21ecaf4fd962f57733078cc
jzhang319 <jzhang319@gmail.com>
2023-11-16 13:23:03 -0500

    finished 020

--- 020addLists.js
+++ 020addLists.js
@@ -23,11 +23,48 @@ class Node {
     this.next = null;
   }
 }
+const addLists = (head1, head2) => {
+  const dummyHead = new Node(null)
+  let tail = dummyHead
 
-const createLinkedList = (values) => {
-  // todo
+  let carry = 0
+  let current1 = head1
+  let current2 = head2
 
-};
+  while (current1 !== null || current2 !== null || carry === 1){
+    const val1 = current1 === null ? 0 : current1.val
+    const val2 = current2 === null ? 0 : current2.val
+    const sum = val1 + val2 + carry
+    carry = sum > 9 ? 1 : 0
+    const digit = sum % 10
+
+    if (current1 !== null) current1 = current1.next
+    if (current2 !== null) current2 = current2.next
+
+    tail.next = new Node(digit)
+    tail = tail.next
+  }
+  return dummyHead.next
+}
+
+// recursion
+// const addLists = (head1, head2, carry =0) => {
+//   if(head1 === null && head2 === null && carry === 0) return null
+//   const val1 = head1 === null ? 0 : head1.val
+//   const val2 = head2 === null ? 0 : head2.val
+
+//   const sum = val1 + val2 + carry
+//   const nextCarry = sum > 9 ? 1 : 0;
+//   const digit = sum % 10
+
+//   const resultNode = new Node(digit)
+
+//   const next1 = head1 === null ? null : head1.next
+//   const next2 = head2 === null ? null : head2.next
+
+//   resultNode.next = addLists(next1, next2, nextCarry)
+//   return resultNode
+// };
 
 addLists(a1, b1);
 // 5 -> 7 -> 9

b070d812f8f662c0e341cafcef0eea48dfc9928c
jzhang319 <jzhang319@gmail.com>
2023-11-15 23:19:12 -0500

    updated 15

--- 015mergeList.js
+++ 015mergeList.js
@@ -11,6 +11,18 @@ d.next = e;
 e.next = f;
 // 5 -> 7 -> 10 -> 12 -> 20 -> 28
 
+const q = new Node(6);
+const r = new Node(8);
+const s = new Node(9);
+const t = new Node(25);
+q.next = r;
+r.next = s;
+s.next = t;
+// 6 -> 8 -> 9 -> 25
+
+mergeLists(a, q);
+// 5 -> 6 -> 7 -> 8 -> 9 -> 10 -> 12 -> 20 -> 25 -> 28
+
 class Node {
   constructor(val) {
     this.val = val;

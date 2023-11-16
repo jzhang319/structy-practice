cff931ebb937b2e54809c0c4bd34edf70f36f7d5
jzhang319 <jzhang319@gmail.com>
2023-11-15 23:45:26 -0500

    starting 16

---
+++ 016isUnivalueList.js
@@ -0,0 +1,22 @@
+const a = new Node(7);
+const b = new Node(7);
+const c = new Node(7);
+
+a.next = b;
+b.next = c;
+
+// 7 -> 7 -> 7
+
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
+
+const isUnivalueList = (head) => {
+  // todo
+  
+};
+
+isUnivalueList(a); // true

58e17fa00f30abeaac0fb0681b3277630af9f558
jzhang319 <jzhang319@gmail.com>
2023-11-16 23:20:20 -0500

    finished 021

--- 021depthFirstValues.js
+++ 021depthFirstValues.js
@@ -17,18 +17,39 @@ c.right = f;
 //  / \     \
 // d   e     f
 
-// class Node {
-//   constructor(val) {
-//     this.val = val;
-//     this.left = null;
-//     this.right = null;
-//   }
-// }
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
 
+// recursion
 const depthFirstValues = (root) => {
-  // todo
+  if (root === null) return [];
+  const leftValues = depthFirstValues(root.left);
+  const rightValues = depthFirstValues(root.right);
+  return ([root.val, ...leftValues, ...rightValues]);
+}
 
-};
+
+// const depthFirstValues = (root) => {
+//   // todo
+//   if (root === null) return [];
+
+//   const result = [];
+//   const stack = [root];
+//   while (stack.length > 0) {
+//     const current = stack.pop();
+//     console.log(current.val);
+//     result.push(current.val);
+
+//     if (current.right) stack.push(current.right);
+//     if (current.left) stack.push(current.left);
+//   }
+//   return result;
+// };
 
 depthFirstValues(a);
 //    -> ['a', 'b', 'd', 'e', 'c', 'f']

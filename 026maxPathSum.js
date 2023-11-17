433f338c5a9b1ed8e1ca7ad2459c5259aefbabba
jzhang319 <jzhang319@gmail.com>
2023-11-17 00:23:29 -0500

    finished 026

--- 026maxPathSum.js
+++ 026maxPathSum.js
@@ -1,21 +1,27 @@
-const a = new Node(3);
+const a = new Node(5);
 const b = new Node(11);
-const c = new Node(4);
-const d = new Node(4);
-const e = new Node(-2);
+const c = new Node(54);
+const d = new Node(20);
+const e = new Node(15);
 const f = new Node(1);
+const g = new Node(3);
 
 a.left = b;
 a.right = c;
 b.left = d;
 b.right = e;
-c.right = f;
+e.left = f;
+e.right = g;
 
-//       3
-//    /    \
-//   11     4
-//  / \      \
-// 4   -2     1
+//        5
+//     /    \
+//    11    54
+//  /   \
+// 20   15
+//      / \
+//     1   3
+
+maxPathSum(a); // -> 59
 
 // class Node {
 //   constructor(val) {
@@ -25,8 +31,13 @@ c.right = f;
 //   }
 // }
 
+// sum of the path with the largest sum from root to leaf node
 const maxPathSum = (root) => {
   // todo
+  if (root === null) return -Infinity;
+  if (root.left === null && root.right === null) return root.val;
+  const maxChildSum = Math.max(maxPathSum(root.left),maxPathSum(root.right))
+  return root.val + maxChildSum
 };
 
 maxPathSum(a); // -> 18

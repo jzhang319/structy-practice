89c353457e9b0f329d492ebfd77da5f1c6c88a4b
jack <jzhang319@gmail.com>
2023-11-26 21:12:13 -0500

    004 again

---
+++ 004mostFrequentChar_again.js
@@ -0,0 +1,23 @@
+const mostFrequentChar = (s) => {
+  const map = {};
+  let maxCount = 0;
+  let maxChar;
+  for (let char of s) {
+    if (!(char in map)) {
+      map[char] = 0;
+    }
+    map[char]++;
+  }
+  for (let char in map) {
+    if(map[char] > maxCount) {
+      maxCount = map[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
+}
+
+console.log(mostFrequentChar('bookeeper')); // -> 'e'
+console.log(mostFrequentChar('david')); // -> 'd'
+console.log(mostFrequentChar('abby')); // -> 'b'
+console.log(mostFrequentChar('mississippi')); // -> 'i'

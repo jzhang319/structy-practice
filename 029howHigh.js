064623349ffcc63bb6f79885b2c28abcf91230e1
jzhang319 <jzhang319@gmail.com>
2023-11-29 14:51:06 -0500

    finished 029

--- 029howHigh.js
+++ 029howHigh.js
@@ -1,9 +1,9 @@
-const a = new Node('a');
-const b = new Node('b');
-const c = new Node('c');
-const d = new Node('d');
-const e = new Node('e');
-const f = new Node('f');
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+const e = new Node("e");
+const f = new Node("f");
 
 a.left = b;
 a.right = c;
@@ -27,6 +27,12 @@ c.right = f;
 
 const howHigh = (node) => {
   // todo
+  if (node === null) return -1;
+
+  const leftTreeHeight = howHigh(node.left);
+  const rightTreeHeight = howHigh(node.right);
+
+  return 1 + Math.max(leftTreeHeight, rightTreeHeight);
 };
 
 howHigh(a); // -> 2

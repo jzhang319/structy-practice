cefc14e980c9c6520811e3a02e26365abe1999de
jzhang319 <jzhang319@gmail.com>
2024-02-06 14:44:46 -0500

    021 python edit

---
+++ 021depthFirstValues.py
@@ -0,0 +1,35 @@
+# class Node:
+#   def __init__(self, val):
+#     self.val = val
+#     self.left = None
+#     self.right = None
+
+# iterative solution
+
+def depth_first_values(root):
+  if root is None:
+    return []
+
+  values = []
+  stack = [root]
+
+  while stack:
+    current = stack.pop()
+    values.append(current.val)
+
+    if current.right is not None:
+      stack.append(current.right)
+    if current.left is not None:
+      stack.append(current.left)
+  return values
+
+
+# recursive solution
+# def depth_first_values(root):
+#   if root is None:
+#     return []
+
+#   left_values = depth_first_values(root.left)
+#   right_values = depth_first_values(root.right)
+
+#   return [root.val, *left_values, *right_values]

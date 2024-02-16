7500b8bf5c5c6057e9f05a7aaf9022c0ae1ed74a
jzhang319 <jzhang319@gmail.com>
2024-02-16 16:15:24 -0500

    finished 029 python

--- 029how_high.py
+++ 029how_high.py
@@ -1,5 +1,8 @@
 def how_high(node):
   if node is None:
-    return 0
+    return -1
 
-  
+  left_tree_height = how_high(node.left)
+  right_tree_height = how_high(node.right)
+
+  return 1 + max(left_tree_height, right_tree_height)

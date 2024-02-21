be9fc6c8cf96d1b22fbbd0b3914eff444acfdbad
jzhang319 <jzhang319@gmail.com>
2024-02-21 16:58:54 -0500

    finished 030 python version

--- 030bottom_right_value.py
+++ 030bottom_right_value.py
@@ -4,5 +4,18 @@
 #     self.left = None
 #     self.right = None
 
+from collections import deque
+
 def bottom_right_value(root):
-  pass # todo
+  queue = deque([root])
+
+  while queue:
+    current = queue.popleft()
+
+    if current.left is not None:
+      queue.append(current.left)
+
+    if current.right is not None:
+      queue.append(current.right)
+
+  return current.val

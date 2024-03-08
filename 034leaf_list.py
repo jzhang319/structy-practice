3e716cffa9a85f17d57e2c5203e128a1021b221c
jzhang319 <jzhang319@gmail.com>
2024-03-08 13:10:51 -0500

    finished 034

--- 034leaf_list.py
+++ 034leaf_list.py
@@ -4,5 +4,38 @@
 #     self.left = None
 #     self.right = None
 
+# a = Node("a")
+# b = Node("b")
+# c = Node("c")
+# d = Node("d")
+# e = Node("e")
+# f = Node("f")
+
+# a.left = b
+# a.right = c
+# b.left = d
+# b.right = e
+# c.right = f
+
 def leaf_list(root):
-  pass # todo
+  if root is None:
+    return []
+
+  stack = [ root ]
+  result = []
+
+  while stack:
+    current = stack.pop()
+
+    if current.left is None and current.right is None:
+      result.append(current.val)
+
+    if current.right is not None:
+      stack.append(current.right)
+
+    if current.left is not None:
+      stack.append(current.left)
+
+
+  return result
+

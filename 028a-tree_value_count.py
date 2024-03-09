3ffed57d31cb484c85e4624610965039e83712e5
jzhang319 <jzhang319@gmail.com>
2024-03-09 16:25:18 -0500

    finished 028a

--- 028a-tree_value_count.py
+++ 028a-tree_value_count.py
@@ -5,4 +5,23 @@
 #     self.right = None
 
 def tree_value_count(root, target):
-  
+  if root is None:
+    return 0
+
+  queue = deque( [root] )
+
+  count = 0
+
+  while queue:
+    current = queue.popleft()
+
+    if current.val == target:
+      count += 1
+
+    if current.right is not None:
+      queue.append(current.right)
+
+    if current.left is not None:
+      queue.append(current.left)
+
+  return count

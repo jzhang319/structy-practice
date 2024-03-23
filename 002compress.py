439e4d3324b1c99dbd1d7eea116eba94af2a625e
jzhang319 <jzhang319@gmail.com>
2024-03-23 13:50:24 -0400

    update 002 python version

--- 002compress.py
+++ 002compress.py
@@ -1,7 +1,21 @@
 def compress(s):
+  s += '!'
   result = []
   j = 0
   i = 0
 
   while j < len(s):
-    
+    if s[i] == s[j]:
+      j += 1
+    else:
+      num = j - i
+
+      if num == 1:
+        result.append( s[i] )
+      else:
+        result.append( str(num))
+        result.append(s[i] )
+        
+      i = j
+
+  return ''.join(result)

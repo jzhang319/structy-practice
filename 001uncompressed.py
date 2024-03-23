5e3d074823d9c501a68c5089e761887732d25abc
jzhang319 <jzhang319@gmail.com>
2024-03-23 13:34:37 -0400

    redo 001 in python

---
+++ 001uncompressed.py
@@ -0,0 +1,14 @@
+def uncompressed(s):
+    numbers = '0123456789'
+    result = []
+    i = 0
+    j = 0
+    while j < len(s):
+      if s[j] in numbers:
+        j += 1
+      else:
+        num = int(s[i:j])
+        result.append(s[j] * num)
+        j += 1
+        i = j
+    return result

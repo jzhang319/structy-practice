346bf22a7914c3b6f9bb1bb17b45236bdaed5723
jzhang319 <jzhang319@gmail.com>
2024-03-30 14:36:56 -0400

    007 , more efficient

--- 007intersection.py
+++ 007intersection.py
@@ -1,3 +1,4 @@
+# brute force solution
 def intersection(a, b):
   result = []
 
@@ -6,3 +7,8 @@ def intersection(a, b):
       result.append(item)
 
   return result
+
+# more efficient
+def intersection(a, b):
+  set_a = set(a)
+  return [ item for item in b if item in set_a ]

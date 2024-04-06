93e5095939face00257c1c6f428ee268c3f7427a
jzhang319 <jzhang319@gmail.com>
2024-04-06 18:04:33 -0400

    011 python version

---
+++ 011linked_list_find.py
@@ -0,0 +1,14 @@
+# class Node:
+#   def __init__(self, val):
+#     self.val = val
+#     self.next = None
+
+def linked_list_find(head, target):
+  current = head
+
+  while current is not None:
+    if current.val == target:
+      return True
+    current = current.next
+
+  return False

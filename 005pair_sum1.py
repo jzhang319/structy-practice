6de41058462b17bf210dd340a00bf7551f64adb2
jzhang319 <jzhang319@gmail.com>
2024-04-20 18:24:04 -0400

    005 practice again -python

---
+++ 005pair_sum1.py
@@ -0,0 +1,9 @@
+def pair_sum(numbers, target_sum):
+  previous_numbers = {}
+
+  for index, num in enumerate(numbers):
+    complement = target_sum - num
+
+    if complement in previous_numbers:
+      return (previous_numbers[complement], index)
+    previous_numbers[num] = index

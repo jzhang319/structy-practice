71d80a7c34f8d1e400940d5c42698a14b439bfa0
jzhang319 <jzhang319@gmail.com>
2024-04-20 15:08:35 -0400

    003 practice again -python

---
+++ 003annagram1.py
@@ -0,0 +1,12 @@
+def anagram(s1, s2):
+  return char_count(s1) == char_count(s2)
+
+def char_count(s):
+  count = {}
+  for char in s:
+    if char not in count:
+      count[char] = 0
+
+    count[char] += 1
+
+  return count

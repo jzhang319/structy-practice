e26aa526235851eabb89de8d1593f98fda09677f
jzhang319 <jzhang319@gmail.com>
2024-04-20 15:11:28 -0400

    004 practice again -python

---
+++ 004most_frequent_char1.py
@@ -0,0 +1,14 @@
+def most_frequent_char(s):
+  count = {}
+  for char in s:
+    if char not in count:
+      count[char] = 0
+
+    count[char] += 1
+
+  max_cha = None
+  for char in s:
+    if max_char is None or count[char] > count[max_char]:
+      max_char = char
+
+  return max_char

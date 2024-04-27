68e10f0a58c547bc7d6b8f69303b6b00e9079751
jzhang319 <jzhang319@gmail.com>
2024-04-27 15:43:17 -0400

    practice 007 python again

---
+++ 007intersection1.py
@@ -0,0 +1,13 @@
+def intersection(a, b):
+  result = []
+
+  for item in b:
+    if item in a:
+      result.append(item)
+
+  return result
+
+# more efficient
+def intersection(a, b):
+  set_a = set(a)
+  return [ item for item in b if item in set_a ]

d51444cc48fa86b96d504b36dff609999a8de22d
jzhang319 <jzhang319@gmail.com>
2024-05-14 15:25:31 -0400

    019 python version

--- 018remove_node.py
+++ 018remove_node.py
@@ -3,6 +3,7 @@
 #     self.val = val
 #     self.next = None
 
+# iterative solution
 def remove_node(head, target_val):
   current = head
   prev = None
@@ -19,3 +20,22 @@ def remove_node(head, target_val):
     current = current.next
 
   return head
+
+# n = number of nodes
+# Time: O(n)
+# Space: O(1)
+
+# # recursive solution
+# def remove_node(head, target_val):
+#   if head is None:
+#     return None
+
+#   if head.val == target_val:
+#     return head.next
+
+#   head.next = remove_node(head.next, target_val)
+#   return head
+
+# n = number of nodes
+# Time: O(n)
+# Space: O(n)

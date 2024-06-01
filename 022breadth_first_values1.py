b74f7de88b4377c1e1bff2b7839931be4e13c6ae
jzhang319 <jzhang319@gmail.com>
2024-05-31 22:15:53 -0400

    redoing 022 python version

--- 022breadth_first_values1.py
+++ 022breadth_first_values1.py
@@ -1,13 +1,12 @@
 from collections import deque
 
- 
+
+
 class Node:
-    def __init__(self, val):
-      self.val = val
-      self.left = None
-      self.right = None
-
-
+  def __init__(self, val):
+    self.val = val
+    self.left = None
+    self.right = None
 
 
 def breadth_first_values(root):
